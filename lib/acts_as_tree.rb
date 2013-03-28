908e22de2ff5fe7ac28a10473f1ad54a9ae91219
Mischa Berger <mischa78@xs4all.nl>
2013-03-28 12:10:55 +0900

    Fix Rails 4 deprecation warning

--- lib/acts_as_tree.rb
+++ lib/acts_as_tree.rb
@@ -75,9 +75,9 @@ module ActsAsTree
         counter_cache: configuration[:counter_cache],
         inverse_of:    :children
 
-      has_many :children, class_name:  name,
+      has_many :children, -> { order configuration[:order] },
+        class_name:  name,
         foreign_key: configuration[:foreign_key],
-        order:       configuration[:order],
         dependent:   configuration[:dependent],
         inverse_of:  :parent
 
